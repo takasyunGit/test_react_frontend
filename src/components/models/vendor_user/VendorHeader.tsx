f8fb0b385bd63d1f33ed155b01a4cdece931f924
shun0610 <graphic228@yahoo.co.jp>
2024-03-09 11:26:12 +0900

    [add] ベンダー側のヘッダー作成 現在はユーザー側と同じ構成だが将来的に異なっていくことを考えて既存のコンポーネントとは別に作成している。

---
+++ src/components/models/vendor_user/VendorHeader.tsx
@@ -0,0 +1,102 @@
+import React, {useContext} from "react"
+import { useNavigate, Link as RouterLink} from "react-router-dom"
+import Cookies from "js-cookie"
+
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import IconButton from "@mui/material/IconButton"
+import MenuIcon from "@mui/icons-material/Menu"
+
+import { signOut } from "models/vendor_user/auth"
+import { AuthVendorUserContext } from "components/models/vendor_user/AuthVendorUserProvider"
+
+const VendorHeader: React.FC = () => {
+  const { loading, isSignedIn, setIsSignedIn} = useContext(AuthVendorUserContext)
+  const navigate = useNavigate()
+
+  const handleSignOut = async(e: React.MouseEvent<HTMLButtonElement>) => {
+    try{
+      const res = await signOut()
+
+      if (res?.data.success === true) {
+        Cookies.remove("_access_token_v")
+        Cookies.remove("_client_v")
+        Cookies.remove("_uid_v")
+
+        setIsSignedIn(false)
+        navigate("/vendor/signin")
+        console.log("Succeeded in sign out")
+      } else {
+        console.log("Failed in sign out")
+      }
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  const AuthButton = () => {
+    if (!loading) {
+      if (isSignedIn) {
+        return (
+          <>
+            <Button
+              color="inherit"
+              sx={{textTransform: "none"}}
+              onClick={handleSignOut}
+            >
+              Sign out
+            </Button>
+          </>
+        )
+      } else {
+        return (
+          <>
+            <Button
+              component={RouterLink}
+              to="/vendor/signin"
+              sx={{textTransform: "none"}}
+              color="inherit"
+            >
+              Sign in
+            </Button>
+          </>
+        )
+      }
+    } else {
+      return <></>
+    }
+  }
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            sx={{marginRight: (theme) => theme.spacing(2)}}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component={RouterLink}
+            to="/vendor"
+            variant="h6"
+            sx={{
+              flexGlow: 1,
+              textDecoration: "none",
+              color: "inherit"
+            }}
+          >
+            Sample
+          </Typography>
+          <AuthButton />
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default VendorHeader
--- src/components/ui/CommonLayout.tsx
+++ src/components/ui/CommonLayout.tsx
@@ -1,21 +1,29 @@
 import React from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 
 import { Container, Grid } from "@mui/material"
 import { styled } from "@mui/material/styles"
 
 import UserHeader from "components/models/user/UserHeader"
+import VendorHeader from "components/models/vendor_user/VendorHeader"
 
 const DivContainer = styled(Container)({
   marginTop: "3rem",
   maxWidth: "lg"
 })
 
-interface CommonLayoutProps {
-  children: React.ReactElement
-}
-
 const CommonLayout = () => {
+  const currentPathName = useLocation().pathname
+  const vendorPathPattern = /^\/vendor/
+
+  const Header: React.FC = () => {
+    if (currentPathName.match(vendorPathPattern)) {
+      return(<VendorHeader />)
+    } else {
+      return(<UserHeader />)
+    }
+  }
+
   return(
     <>
       <header>
