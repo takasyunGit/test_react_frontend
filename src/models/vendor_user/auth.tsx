c8ac94c82b2ec67802fca8fdc543b7e7852839f4
shun0610 <graphic228@yahoo.co.jp>
2024-03-09 11:24:06 +0900

    [add] ベンダー側のサインイン＆サインアウトの関数作成

--- src/models/vendor_user/auth.tsx
+++ src/models/vendor_user/auth.tsx
@@ -3,23 +3,23 @@ import Cookies from "js-cookie"
 import { SignInParams } from "models/vendor_user/type"
 
 export const signIn = (params: SignInParams) => {
-  return client.post("vendor/sign_in", params)
+  return client.post("vendor_user/sign_in", params)
 }
 
 export const signOut = () => {
-  return client.delete("vendor/sign_out", {headers: {
-    "access-token": Cookies.get("_access_token"),
-    "client": Cookies.get("_client"),
-    "uid": Cookies.get("_uid")
+  return client.delete("vendor_user/sign_out", {headers: {
+    "access-token": Cookies.get("_access_token_v"),
+    "client": Cookies.get("_client_v"),
+    "uid": Cookies.get("_uid_v")
   }})
 }
 
 export const getCurrentVendorUser = () => {
-  if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
+  if (!Cookies.get("_access_token_v") || !Cookies.get("_client_v") || !Cookies.get("_uid_v")) return
 
-  return client.get("/vendor/sessions", { headers: {
-    "access-token": Cookies.get("_access_token"),
-    "client": Cookies.get("_client"),
-    "uid": Cookies.get("_uid")
+  return client.get("/vendor_user/sessions", { headers: {
+    "access-token": Cookies.get("_access_token_v"),
+    "client": Cookies.get("_client_v"),
+    "uid": Cookies.get("_uid_v")
   }})
 }
