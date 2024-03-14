a470f50b5c983613c73a8d5fade8dc3ac04443a4
shun0610 <graphic228@yahoo.co.jp>
2024-03-14 14:47:21 +0900

    [add]userOfferに関する定数のリストとtype追加 都道府県とユーザー希望の種別

---
+++ src/utils/type.ts
@@ -0,0 +1,13 @@
+type Enumerate<Max extends number, IncrementalNumbers extends number[] = []> =
+  IncrementalNumbers['length'] extends Max ? IncrementalNumbers[number] : Enumerate<Max, [...IncrementalNumbers, IncrementalNumbers['length']]>
+
+type IntRange<Min extends number, Max extends number> =
+  Exclude<Enumerate<Max>, Enumerate<Min>> | Max
+
+export type PrefectureCode = IntRange<1, 47>
+
+export type UserOfferRequestTypeCode = 1 | 2 | 3
+
+export type NumberCodeListType = {
+  [key: number]: string;
+}
