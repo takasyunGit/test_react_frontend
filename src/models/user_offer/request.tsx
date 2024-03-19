2d9958ab0ecc77eeea38f604a3f45542d738b4b8
shun0610 <graphic228@yahoo.co.jp>
2024-03-19 19:24:32 +0900

    [add]Vendor側にUseOffer取得機能追加

--- src/models/user_offer/request.tsx
+++ src/models/user_offer/request.tsx
@@ -31,3 +31,13 @@ export const getUserOfferList = () => {
     "uid": Cookies.get("_uid")
   }})
 }
+
+export const vendorGetUserOfferList = () => {
+  if (!Cookies.get("_access_token_v") || !Cookies.get("_client_v") || !Cookies.get("_uid_v")) return
+
+  return client.get("vendor_user/user_offers/", { headers: {
+    "access-token": Cookies.get("_access_token_v"),
+    "client": Cookies.get("_client_v"),
+    "uid": Cookies.get("_uid_v")
+  }})
+}
