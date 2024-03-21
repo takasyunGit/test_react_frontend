06832105302442a72b150cf9eafb9fbec00b207f
shun0610 <graphic228@yahoo.co.jp>
2024-03-21 17:59:49 +0900

    [add]VendorOfferの作成API追加

---
+++ src/models/vendor_offer/request.tsx
@@ -0,0 +1,13 @@
+import client from "utils/client"
+import Cookies from "js-cookie"
+import { CreateVendorOfferParams } from "models/vendor_offer/type"
+
+export const createVendorOffer = (params: CreateVendorOfferParams) => {
+  if (!Cookies.get("_access_token_v") || !Cookies.get("_client_v") || !Cookies.get("_uid_v")) return
+
+  return client.post("vendor_user/vendor_offers", params, { headers: {
+    "access-token": Cookies.get("_access_token_v"),
+    "client": Cookies.get("_client_v"),
+    "uid": Cookies.get("_uid_v")
+  }})
+}
