c0689c0434a0ffb2de2fc7a9f1ba0bff9b334c0a
shun0610 <graphic228@yahoo.co.jp>
2024-03-21 17:59:26 +0900

    [add]VendorOfferのtype追加

---
+++ src/models/vendor_offer/type.ts
@@ -0,0 +1,17 @@
+
+export type VendorOffer = {
+  id: number,
+  vendorUserId: number,
+  userOfferId: number,
+  estimate: number,
+  remark: string,
+  createdAt: string,
+  updatedAt: string
+}
+
+export type CreateVendorOfferParams = Pick<
+    VendorOffer,
+    "userOfferId" |
+    "estimate" |
+    "remark"
+  >
