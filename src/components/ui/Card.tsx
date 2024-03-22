16ce98c860772cc22e992d16d80250350296f2c8
shun0610 <graphic228@yahoo.co.jp>
2024-03-22 18:24:17 +0900

    [add]CardContentをラップしたものを追加。コンテンツがない場合にメッセージを表示するもの

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react"
+import CardContent from "@mui/material/CardContent"
+
+type CardContentProps = {
+  resStatus: number,
+  children: ReactNode
+  message?: string
+}
+
+export const ShowResCardContent = (props: CardContentProps) => {
+  if (props.resStatus === 200) {
+    return(
+      <CardContent>
+        {props.children}
+      </CardContent>
+    )
+  }
+  return (
+    <CardContent>
+      {props.message ? props.message : "An unexpected error has occurred"}
+    </CardContent>
+  )
+}
