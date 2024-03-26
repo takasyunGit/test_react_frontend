256df0cdf708e60a9b0aa158c525b88439fa4496
shun0610 <graphic228@yahoo.co.jp>
2024-03-26 17:08:50 +0900

    [front][add]エラー表示するためのコンポーネント追加

---
+++ src/components/ui/DisplayErrors.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+type Props = {
+  errors?: any,
+  children: React.ReactNode
+ }
+
+export const DisplayErrors: React.FC<Props> = (props) => {
+  const { errors, children } = props
+
+  if (!errors){
+    return(
+      <>
+        {children}
+      </>
+    )
+  }
+  if (errors?.response?.status === 404){
+    return(
+      <>
+        ページが見つかりませんでした。
+      </>
+    )
+  }
+  return(
+    <>
+      予期せぬエラーが発生しました。
+    </>
+  )
+}
--- src/utils/detectErrors.tsx
+++ src/utils/detectErrors.tsx
@@ -1,8 +1,11 @@
+import { useContext } from "react"
 import Axios from 'axios'
 
 export const detectAxiosErrors = (e: any, setMessageOpen?: Function, setMessage?: Function) => {
+
   if (Axios.isAxiosError(e) && e.response && e.response.status === 404) {
-    window.location.href = "/Page404"
+    console.log(e)
+    console.log("ページが見つかりませんでした。")
     return
   }
   if (Axios.isAxiosError(e) && e.response && e.response.status === 422 && setMessage && setMessageOpen) {
@@ -21,5 +24,6 @@ export const detectAxiosErrors = (e: any, setMessageOpen?: Function, setMessage?
   console.log(e)
   if (setMessage) { setMessage("An unexpected error has occurred") }
   if (setMessageOpen) { setMessageOpen(true) }
+
   return
 }
