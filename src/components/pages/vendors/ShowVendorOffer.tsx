c317ee5b7f7c2d0767e20245f6adee1c0a90022b
shun0610 <graphic228@yahoo.co.jp>
2024-04-03 12:41:33 +0900

    [front][update]共通化した部分を修正

--- src/components/pages/vendors/ShowVendorOffer.tsx
+++ src/components/pages/vendors/ShowVendorOffer.tsx
@@ -1,47 +1,10 @@
-import React, { useState, useEffect } from "react"
-import { useNavigate, useParams } from "react-router-dom"
+import React from "react"
 
-import { signedInCookiesSetter } from "utils/client"
-import { getVendorOffer } from "models/vendor_offer/request"
-import { ShowVendorOfferType } from "models/vendor_offer/type"
-import { detectAxiosErrors } from "utils/detectErrors"
 import ShowVendorOfferCommon from "components/pages/common/ShowVendorOfferCommon"
-import { DisplayErrors } from "components/ui/DisplayErrors"
 
 const ShowVednorOffer: React.FC = () => {
-  const navigate = useNavigate()
-  const params = useParams()
-  const [offerLoading, setOfferLoading] = useState<boolean>(true)
-  const [resStatus, setResStatus] = useState<number>()
-  const [vendorOffer, setVendorOffer] = useState<ShowVendorOfferType | undefined>()
-  const [errors, setErrors] = useState<any>()
-
-  const handleGetvendorOffer = async () => {
-    try{
-      const res = await getVendorOffer(params.vendor_offer_id as string)
-
-      if (!res) { return navigate("/vendor/signin") }
-      signedInCookiesSetter(res, "Vendor")
-
-      if (res && res.status === 200) {
-        setVendorOffer(res!.data.data)
-        setResStatus(res.status)
-      } else {
-        console.log("An unexpected error has occurred")
-      }
-    } catch(e) {
-      setErrors(e)
-      detectAxiosErrors(e)
-    }
-    setOfferLoading(false)
-  }
-
-  useEffect(() => {handleGetvendorOffer()}, [])
-
   return (
-    <DisplayErrors errors={errors}>
-      <ShowVendorOfferCommon vendorOffer={vendorOffer} offerLoading={offerLoading} resStatus={resStatus} />
-    </DisplayErrors>
+    <ShowVendorOfferCommon signInType="Vendor"/>
   )
 }
 
