3dee8647c77364f7617d4e2f41b1968d1e82f401
shun0610 <graphic228@yahoo.co.jp>
2024-04-03 12:39:30 +0900

    [front][update]vendorOffer取得APIの共通化 ベンダーとユーザーで挙動に差が生まれにくいと考えたため共通化した。

--- src/models/vendor_offer/request.tsx
+++ src/models/vendor_offer/request.tsx
@@ -1,21 +1,12 @@
 import client from "utils/client"
 import Cookies from "js-cookie"
 import { CreateVendorOfferParams } from "models/vendor_offer/type"
+import { SignInType } from "utils/type"
 
 export const createVendorOffer = (params: CreateVendorOfferParams) => {
   if (!Cookies.get("_access_token_v") || !Cookies.get("_client_v") || !Cookies.get("_uid_v")) return
 
   return client.post("vendor_user/vendor_offers", params, { headers: {
-    "access-token": Cookies.get("_access_token_v"),
-    "client": Cookies.get("_client_v"),
-    "uid": Cookies.get("_uid_v")
-  }})
-}
-
-export const getVendorOffer = (params: string) => {
-  if (!Cookies.get("_access_token_v") || !Cookies.get("_client_v") || !Cookies.get("_uid_v")) return
-
-  return client.get("vendor_user/vendor_offers/" + params, { headers: {
     "access-token": Cookies.get("_access_token_v"),
     "client": Cookies.get("_client_v"),
     "uid": Cookies.get("_uid_v")
@@ -38,13 +29,29 @@ export const getVendorOfferList = (userOfferId: string, keyId: number | null = n
   })
 }
 
-export const userGetVendorOffer = (params: string) => {
-  if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
+export const getVendorOffer = (params: string, signInType: SignInType) => {
+  let accessToken, headerClient, uid, path
 
-  return client.get("user/vendor_offers/" + params, { headers: {
-    "access-token": Cookies.get("_access_token"),
-    "client": Cookies.get("_client"),
-    "uid": Cookies.get("_uid")
+  if (signInType == "User") {
+    if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
+
+    accessToken = Cookies.get("_access_token")
+    headerClient = Cookies.get("_client")
+    uid = Cookies.get("_uid")
+    path = "user"
+  } else {
+    if (!Cookies.get("_access_token_v") || !Cookies.get("_client_v") || !Cookies.get("_uid_v")) return
+
+    accessToken = Cookies.get("_access_token_v")
+    headerClient = Cookies.get("_client_v")
+    uid = Cookies.get("_uid_v")
+    path = "vendor_user"
+  }
+
+  return client.get(path + "/vendor_offers/" + params, { headers: {
+    "access-token": accessToken,
+    "client": headerClient,
+    "uid": uid
   }})
 }
 
