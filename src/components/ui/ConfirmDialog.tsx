d3e5ba6d0f203344504b3e6e429a65f020424ae7
shun0610 <graphic228@yahoo.co.jp>
2024-04-15 18:35:40 +0900

    [front][add]確認用ダイアログ作成

---
+++ src/components/ui/ConfirmDialog.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useCallback } from 'react'
+
+import CloseIcon from '@mui/icons-material/Close'
+import { Button, Dialog, DialogContent, DialogContentText, DialogTitle, Typography, IconButton } from '@mui/material'
+
+type Props = {
+  open: boolean
+  headerMessage: string
+  confirmMessage: string
+  buttonString: string
+  onClose?: (submit: boolean) => void
+}
+
+export const ConfirmDialog: React.FC<Props> = ({open, headerMessage, confirmMessage, buttonString, onClose}: Props) => {
+  const handleClose = useCallback(
+    (submit: boolean) => {
+      onClose?.(submit)
+    },
+    [onClose]
+  )
+
+  return (
+    <Dialog open={open} onClose={() => handleClose(false)}>
+      <DialogTitle sx={{ m: 0, p: 2 }}>
+        {headerMessage}
+        <IconButton
+          aria-label="close"
+          onClick={() => handleClose(false)}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+
+      <DialogContent>
+        <DialogContentText>
+          {confirmMessage}
+        </DialogContentText>
+        <Button
+          variant="contained"
+          color="error"
+          sx={{ width: '100%' }}
+          onClick={() => handleClose(true)}
+        >
+          {buttonString}
+        </Button>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default ConfirmDialog
