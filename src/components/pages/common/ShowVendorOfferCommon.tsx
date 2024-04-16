0a0ae6c73c0ba965cbba1ff355c7f28de3202329
shun0610 <graphic228@yahoo.co.jp>
2024-04-16 15:30:42 +0900

    [front][update]Avatarで使うメソッドを共通化

--- src/components/pages/common/ShowVendorOfferCommon.tsx
+++ src/components/pages/common/ShowVendorOfferCommon.tsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from "react-router-dom"
 
 import { Card, CardHeader, CardContent, Typography, Button, Avatar, Box, Grid, Pagination } from "@mui/material"
 
-import { AlertMessageContext, AmountForm, DefaultButton, DisplayErrors, OptionalTextField, ProgressCircle, RequiredTextField, initialPaginate } from "@src/components/ui"
+import { AlertMessageContext, AmountForm, DefaultButton, DisplayErrors, OptionalTextField, ProgressCircle, RequiredTextField, initialPaginate, stringAvatar } from "@src/components/ui"
 import { updateVendorOffer, getVendorOffer } from "@src/models/vendor_offer/request"
 import { getVendorOfferChat } from "@src/models/vendor_offer_chat/request"
 import { createVendorOfferChat } from "@src/models/vendor_offer_chat/request"
@@ -124,36 +124,6 @@ const ShowVednorOfferCommon: React.FC<Props> = (props) => {
       colorCode = chat.vendorUserId ? "#fdf5e6" : "#fff"
     }
     return colorCode
-  }
-
-  const stringToColor = (string: string) => {
-    let hash = 0
-    let i
-
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = '#'
-
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff
-      color += `00${value.toString(16)}`.slice(-2);
-    }
-
-    return color
-  }
-
-  const stringAvatar = (name: string | undefined) => {
-    if(!name) return
-
-    const initial = name.split(' ')[0][0] + (name.split(' ')[1]?.[0] || "")
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-      },
-      children: initial,
-    };
   }
 
   const editToggle = () => {
