4c73cf801324c154448488d8843e7825596d19b7
shun0610 <graphic228@yahoo.co.jp>
2024-04-17 19:31:37 +0900

    [front][add]複数画像を送るときようのutil作成

--- src/utils/imageUploaderProps.tsx
+++ src/utils/imageUploaderProps.tsx
@@ -1,3 +1,21 @@
+export const setMultipleUploadAndPreviewImage = (setUploadImage: Function, setPreviewImage: Function, multiple: boolean = false): (e: React.ChangeEvent<HTMLInputElement>) => void => {
+  return (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const fileList = e.target.files
+      const files = Array.from(fileList)
+      let filesWithUniqueKey: {[key: string]: File} = {}
+      let urlWithUniqueKey: {[key: string]: string} = {}
+      files.map((file) => {
+        const url = window.URL.createObjectURL(file)
+        filesWithUniqueKey[url] = file
+        urlWithUniqueKey[url] = url
+      })
+      setUploadImage(filesWithUniqueKey)
+      setPreviewImage(urlWithUniqueKey)
+    }
+  }
+}
+
 export const uploadImage = (setImage: Function): (e: React.ChangeEvent<HTMLInputElement>) => void => {
   return (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
